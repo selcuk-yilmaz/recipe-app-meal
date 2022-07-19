7c6d7640daa74b988c76a9008c9559d9ee8cb9a1
selcuklu <yilmax4@gmail.com>
2022-07-19 12:49:24 +0300

    private

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,16 +2,17 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Cards from "../../components/cards/Cards";
 import Header from "../../components/header/Header";
-import  {ImgDiv,HomeImg, HeaderText} from "./Home.style";
-import homeSvg from "../../assets/home.svg"; 
+import { ImgDiv, HomeImg, HeaderText } from "./Home.style";
+import homeSvg from "../../assets/home.svg";
 
 const Home = () => {
-    const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+  const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
   const [query, setQuery] = useState("egg");
   const [meal, setMeal] = useState(mealType[0]);
   const [recipes, setRecipes] = useState(null);
-  const APP_ID = "48aa453c";
-  const APP_KEY = "aa870390df742d010558b1d0598cfd21";
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_APP_KEY;
+
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
 
   const getData = async () => {
@@ -30,19 +31,26 @@ const Home = () => {
   // useEffect(() => {
   //   getData();
   // }, []);
-// console.log(recipes);
-  return <div>
-<Header setQuery={setQuery} setMeal={setMeal} mealType={mealType} getData={getData} />
-{!recipes && (
-<ImgDiv>
-<HomeImg src={homeSvg} />
-</ImgDiv>
-)}
-{recipes?.length===0 && (
-  <HeaderText>The Food can not be found</HeaderText>
-)}
-{recipes?.length>0 && <Cards recipes={recipes} />}
-  </div>;
+  // console.log(recipes);
+  return (
+    <div>
+      <Header
+        setQuery={setQuery}
+        setMeal={setMeal}
+        mealType={mealType}
+        getData={getData}
+      />
+      {!recipes && (
+        <ImgDiv>
+          <HomeImg src={homeSvg} />
+        </ImgDiv>
+      )}
+      {recipes?.length === 0 && (
+        <HeaderText>The Food can not be found</HeaderText>
+      )}
+      {recipes?.length > 0 && <Cards recipes={recipes} />}
+    </div>
+  );
 };
 
 export default Home;
